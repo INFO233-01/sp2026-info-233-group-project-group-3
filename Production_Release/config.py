2021ff7049e82bef9c000ec7c1874bcc603028c7
giokatat <gkatat@ramapo.edu>
2026-04-21 20:10:39 -0400

    Add files via upload

---
+++ Production_Release/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 19:28:40 2026
+
+@author: katat
+"""
+
+
+CLIENT_ID = "d1d56081e1db4c3f80d9e6ba25fc2683"
+CLIENT_SECRET = "2e75c1e09fe04101884964b2549cf87c"
